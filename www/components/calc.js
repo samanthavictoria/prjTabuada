2608f9f8665d6d63ba4a3c23795eff0b0ec80174
samanthavictoria <sasavicsouza@gmail.com>
2020-06-02 03:39:34 +0900

    quase finalizada, falta arrumar o style

--- www/components/calc.js
+++ www/components/calc.js
@@ -1,2 +1,14 @@
-// This is a JavaScript file
+
+$(document).on("click","#tabuada", function(){
+var numero= $("#num").val();
+var tabuada ='';
+var count;
+
+for(count=0; count<=numero; count++){
+tabuada += (numero +"X"+ count + "=" + (parseInt(numero)*count)+"<br />");
+}
+
+$("#result").html(tabuada);
+});
+
 
